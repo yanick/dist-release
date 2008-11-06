9336f5f5cfc0db64be0efb85cf9f68f05671b110
Yanick Champoux <yanick@babyl.dyndns.org>
2008-11-06 10:52:00 -0500

    minor work

--- lib/Dist/Release/Action.pm
+++ lib/Dist/Release/Action.pm
@@ -5,11 +5,17 @@ use Moose;
 extends 'Dist::Release::Step';
 
 sub check {
+    # its okay for an action not to have a check phase
     $_[0]->diag( 'no check implemented' );
 }
 
 sub release {
     $_[0]->error('release not implemented!');
 }
+
+before release => sub {
+    my $self = shift;
+    $self->set_failed(0);
+};
 
 1;
