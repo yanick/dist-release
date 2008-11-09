3b67c73e6cb7b1b220b3ebb27c79e84d1552c2bd
Yanick Champoux <yanick@babyl.dyndns.org>
2008-11-08 21:39:29 -0500

    can't check for dist created later

--- lib/Dist/Release/Action/CPANUpload.pm
+++ lib/Dist/Release/Action/CPANUpload.pm
@@ -8,27 +8,14 @@ extends 'Dist::Release::Action';
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-sub check { 
-    my ( $self ) = @_;
+sub check {
+    my ($self) = @_;
 
     # do we have a pause id?
-    unless ( $self->distrel->config->{pause}{id} 
-         and $self->distrel->config->{pause}{password} ) {
-        $self->error( 'pause id or password missing from config file' );
+    unless ($self->distrel->config->{pause}{id}
+        and $self->distrel->config->{pause}{password} ) {
+        $self->error('pause id or password missing from config file');
     }
-
-    $self->diag( 'verifying that the tarball is present' );
-
-    my @archives = <*.tar.gz> or return $self->error( 'no tarball found' );
-
-    if ( @archives > 1 ) {
-        return $self->error( 'more than one tarball file found: ' . join ',',
-            @archives );
-    }
-
-    $self->diag( "found tarball: $archives[0]" );
-
-    $self->distrel->stash->{tarball} = $archives[0];
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,17 +23,29 @@ sub check {
 sub release {
     my $self = shift;
 
-    my $tarball = $self->distrel->stash->{tarball};
+    $self->diag('verifying that the tarball is present');
 
-    $self->diag( "uploading tarball '$tarball' to CPAN" );
+    my @archives = <*.tar.gz> or return $self->error('no tarball found');
 
-    my( $id, $password ) = map { $self->distrel->config->{pause}{$_} } 
-        qw/ id password /;
+    if ( @archives > 1 ) {
+        return $self->error( 'more than one tarball file found: ' . join ',',
+            @archives );
+    }
 
-    my $uploader = CPAN::Uploader->new({ user => $id, password => $password });
+    my $tarball = $archives[0];
+
+    $self->diag("found tarball: $tarball");
+
+    $self->diag("uploading tarball '$tarball' to CPAN");
+
+    my ( $id, $password ) =
+      map { $self->distrel->config->{pause}{$_} } qw/ id password /;
+
+    my $uploader =
+      CPAN::Uploader->new( { user => $id, password => $password } );
 
     unless ( $self->distrel->pretend ) {
-        $uploader->upload_file( $tarball );
+        $uploader->upload_file($tarball);
     }
 }
 
