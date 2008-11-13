d6b8abf3415c4f631d0f42f23f124ba6b2255e0d
Yanick Champoux <yanick@babyl.dyndns.org>
2008-11-13 13:14:08 -0500

    adding documentation

--- lib/Dist/Release/Step/Builder.pm
+++ lib/Dist/Release/Step/Builder.pm
@@ -52,3 +52,24 @@ sub release {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Dist::Release::Step::Builder - builder-specific step parent class
+
+=head1 DESCRIPTION
+
+This class is meant to be used as the base class of 
+builder-dependent steps, so that when the step is called,
+its actions are automagically dispatched to the sub-step associated
+with the builder used by the distribution.
+
+More clearly, assuming the generic step is called I<DoStuff>, if 
+'Module::Build' is used then the step module I<DoStuff::Build> is
+going to be used.  If it's 'MakeMaker', then it's going to be
+the module I<DoStuff::MakeMaker>.
+
+
+
+
